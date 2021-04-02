986143597758d94217afdc6da8c0995fec4c822d
Eti Priya <etipriya@gmail.com>
2021-04-02 16:22:32 +0100

    added code to js file

---
+++ assests/js/main.js
@@ -0,0 +1,76 @@
+//Declaring the value for container
+const scheduleContainer = $(".container");
+
+// Declare the function to get time
+const currentTime = () => {
+  const currentDay = $("#currentDay");
+  const currentTime = moment().format("dddd, MMMM Do");
+  currentDay.text(currentTime);
+};
+
+//setInterval(currentTime, 1000);
+// function to add textarea data to local storage
+const renderTextArea = () => {
+  const plannerEvents = JSON.parse(localStorage.getItem("plannerEvents"));
+
+  //Check if data is null
+  if (plannerEvents !== null) {
+    //Declare variable to get current Hour
+    const currentHour = moment().hour();
+
+    // Declare time block array
+    const timeBlockArray = $(".container .row");
+
+    const callback = function () {
+      const textArea = $(this).find("textarea");
+      const timeBlockTime = Number.parseInt($(this).data("time"), 10);
+
+      //Time block to be color coded
+      if (timeBlockTime === currentHour) {
+        textArea.removeClass("past").addClass("present");
+      }
+      if (timeBlockTime > currentHour) {
+        textArea.removeClass("present").addClass("future");
+      }
+
+      const plannedEvent = plannerEvents[timeBlockTime];
+      textArea.text(plannedEvent);
+    };
+    timeBlockArray.each(callback);
+  } else {
+    //Add an empty array into local storage
+    localStorage.setItem("plannerEvents", JSON.stringify({}));
+  }
+};
+
+//on button click get items from local storage into planner
+const onclick = function (event) {
+  const plannerEvents = JSON.parse(localStorage.getItem("plannerEvents"));
+  const target = $(event.target);
+  // const currentTarget = $(event.currentTarget);
+
+  if (target.is("button")) {
+    const key = target.attr("id");
+    const value = target.parent().find("textarea").val();
+
+    const newObject = {
+      ...plannerEvents,
+      [key]: value,
+    };
+    localStorage.setItem("plannerEvents", JSON.stringify(newObject));
+  }
+};
+
+//on ready function get current date and render planner events
+const onReady = () => {
+  //Event Listener
+  $(".container").click(onClick);
+  currentTime();
+  renderTextArea();
+
+  //Current date
+  //renderCurrentDate();
+  //renderCalenderEvents();
+};
+
+$(document).ready(onReady);
